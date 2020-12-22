130c984f8913de78f582802eab762408530cc3b5
MUKESH KUMAR <mukesh.iilm@gmail.com>
2020-12-22 11:28:54 +0530

    Add files via upload

---
+++ 1.Hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 09:51:00 2020
+
+@author: mukesh
+"""
+# This prints Hello World on the output screen
+print('Hello World')
